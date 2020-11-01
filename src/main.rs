4df71aeae4bf6b9c8c2cf0be9e49c1d9bedaceda
Geoffrey Jount <geoffrey.jount@epita.fr>
2020-11-01 04:19:44 +0100

    Implement struct Matrix

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,47 @@
-#[macro_use(c)]
-extern crate cute;
+use crate::matrix::Matrix;
 
-use crate::neuron::Neuron;
-
-mod neuron;
+mod matrix;
 
 
 fn main() {
 
     // Weights
-    let weights = [
-        vec![ 0.2,   0.8, -0.5,   1.0],
-        vec![ 0.5,  -0.91, 0.26, -0.5],
-        vec![-0.26, -0.27, 0.17,  0.87]
-    ];
+    let weights = Matrix {
+        n: 3,
+        p: 4,
+        val: vec![
+            vec![0.2, 0.8, -0.5, 1.0],
+            vec![0.5, -0.91, 0.26, -0.5],
+            vec![-0.26, -0.27, 0.17, 0.87]
+        ]
+    };
 
     // Biases
-    let biases = vec![2.0, 3.0, 0.5];
-
-    // Neurons
-    let n1 = Neuron {
-                size: 4,
-                weights: &weights[0],
-                bias: biases[0]
-    };
-    let n2 = Neuron {
-                size: 4,
-                weights: &weights[1],
-                bias: biases[1]
-    };
-    let n3 = Neuron {
-                size: 4,
-                weights: &weights[2],
-                bias: biases[2]
+    let biases = Matrix {
+        n: 3,
+        p: 3,
+        val: vec![
+            vec![2.0, 3.0, 0.5],
+            vec![2.0, 3.0, 0.5],
+            vec![2.0, 3.0, 0.5]
+        ]
     };
 
     // Inputs
-    let inputs = vec![
-        vec![1.0, 2.0, 3.0, 2.5],
-        vec![2.0, 5.0, -1.0, 2.0],
-        vec![-1.5, 2.7, 3.3, -0.8]
-    ];
+    let inputs = Matrix {
+        n: 3,
+        p: 4,
+        val: vec![
+            vec![1.0, 2.0, 3.0, 2.5],
+            vec![2.0, 5.0, -1.0, 2.0],
+            vec![-1.5, 2.7, 3.3, -0.8]
+        ]
+    };
 
     // Outputs
-    let outputs = vec![
-        c![n1.output(&input), for input in &inputs],
-        c![n2.output(&input), for input in &inputs],
-        c![n3.output(&input), for input in &inputs]
-    ];
+    let outputs = inputs.mult(&weights.transposed()) + biases;
 
     // Display
-    println!("Output : {:?}", outputs);
-
-    // NOTE : output is transposed, since we usually want one column for one neuron,
-    // and one row for one input.
+    println!("Output : {:?}", outputs.val);
 
 }
---
+++ src/matrix.rs
@@ -0,0 +1,63 @@
+use std::ops;
+use std::vec::Vec;
+
+
+#[derive(Debug)]
+pub struct Matrix {
+    pub n : usize,
+    pub p : usize,
+    pub val : Vec<Vec<f32>>
+}
+
+
+impl Matrix {
+    pub fn mult(&self, rhs : &Matrix) -> Matrix {
+        if self.p != rhs.n {
+            panic!("Mind the dimension!");
+        }
+        let mut val = Vec::new();
+        for i in 0..self.n {
+            let mut line = Vec::new();
+            for j in 0..rhs.p {
+                let mut e = 0.0;
+                for k in 0..self.p {
+                    e = e + self.val[i][k] * rhs.val[k][j]
+                }
+                line.push(e);
+            }
+            val.push(line)
+        }
+        return Matrix {n: self.n, p: rhs.p, val: val};
+    }
+
+    pub fn transposed(&self) -> Matrix {
+        let mut val = Vec::new();
+        for j in 0..self.p {
+            let mut line = Vec::new();
+            for i in 0..self.n {
+                line.push(self.val[i][j]);
+            }
+            val.push(line);
+        }
+        return Matrix {n: self.p, p: self.n, val: val};
+    }
+}
+
+impl ops::Add<Matrix> for Matrix {
+    type Output = Matrix;
+
+    fn add(self, rhs: Matrix) -> Matrix {
+        if self.n != rhs.n || self.p != rhs.p {
+            panic!("Mind the dimension!");
+        }
+        let mut val = Vec::new();
+        for i in 0..self.n {
+            let mut line = Vec::new();
+            for j in 0..self.p {
+                line.push(self.val[i][j] + rhs.val[i][j]);
+            }
+            val.push(line);
+        }
+        return Matrix { n: self.n, p: self.p, val: val};
+    }
+}
